e3c9f2254c025110bd1b0dc380fa0a1d12300e9a
Hyunjin Yeo <jfluke1414@gmail.com>
2020-06-04 03:00:40 -0700

    first update

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" data-textdirection="ltr" class="loading">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    
+    <title>Project Dashboard - Robust Free Bootstrap Admin Template</title>
+    <link rel="apple-touch-icon" sizes="60x60" href="{{asset('robust/app-assets/images/ico/apple-icon-60.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('robust/app-assets/images/ico/apple-icon-76.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('robust/app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('robust/app-assets/images/ico/apple-icon-152.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('robust/app-assets/images/ico/favicon.ico')}}">
+    <link rel="shortcut icon" type="image/png" href="{{asset('robust/app-assets/images/ico/favicon-32.png')}}">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-touch-fullscreen" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('robust/app-assets/css/bootstrap.css')}}">
+    <!-- font icons-->
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/fonts/icomoon.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/fonts/flag-icon-css/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/vendors/css/extensions/pace.css')}}">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN ROBUST CSS-->
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/css/app.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/css/colors.css')}}">
+    <!-- END ROBUST CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/css/core/menu/menu-types/vertical-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/css/core/menu/menu-types/vertical-overlay-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{('robust/app-assets/css/core/colors/palette-gradient.css')}}">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{('robust/assets/css/style.css')}}">
+    <!-- END Custom CSS-->
+    
+    
+    
+    
+  </head>
+ 
